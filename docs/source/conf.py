9ab019ca9a07b3e96681fcb99d6446e1550c9963
Domagoj Fijan <domagoj.fijan@gmail.com>
2023-10-20 20:03:43 -0400

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ copyright = "2022, J Tosovic"
 author = "Jelena Tosovic, Domagoj Fijan, Marko Jukic, Urban Bren"
 
 # The full version, including alpha/beta/rc tags
-version = "0.2.0"
-release = "0.2.0"
+version = "0.3.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
